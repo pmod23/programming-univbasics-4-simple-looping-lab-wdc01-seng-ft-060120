9999d5477850fe03dbc0eebad6631f2a90609ecb
Pramod Thomas <pmod23@gmail.com>
2020-05-21 15:33:13 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
-# Write your methods here
+def loop_message_five_times(string)
+  
+5.times do
+  puts string
+end
+
+end
+def loop_message_n_times(string,n)
+  
+  n.times do
+    puts string
+  end
+  
+end
+def output_array(array)
+  array.each do |element|
+    puts element
+  end
+end
+def return_string_array(array)
+  new_array = []
+  array.each do |element|
+    new_array << element.to_s 
+  end
+  new_array
+end
+  
+  
